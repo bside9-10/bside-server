a6338279f90eb3173d4b67441ca11a516ab5fec7
seonghyeoklee <dltjdgur327@gmail.com>
2022-03-12 00:20:38 +0900

    :tada: 프로젝트 초기화

---
+++ src/main/java/com/bside/study/StudyApplication.java
@@ -0,0 +1,19 @@
+package com.bside.study;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+@SpringBootApplication
+public class StudyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudyApplication.class, args);
+	}
+
+	@Bean
+	public BCryptPasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+}
