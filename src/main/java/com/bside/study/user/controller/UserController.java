5365a1b3602751aa711dbfefc29c703ee0cd40cd
seonghyeoklee <dltjdgur327@gmail.com>
2022-05-15 23:56:18 +0900

    마이페이지 총 가용시간 조회

--- src/main/java/com/bside/study/user/controller/UserController.java
+++ src/main/java/com/bside/study/user/controller/UserController.java
@@ -2,16 +2,22 @@ package com.bside.study.user.controller;
 
 import com.bside.study.common.api.ApiResult;
 import com.bside.study.errors.ResourceNotFoundException;
+import com.bside.study.goal.entity.GoalAvailableTime;
+import com.bside.study.goal.repository.GoalAvailableTimeRepository;
 import com.bside.study.security.CurrentUser;
 import com.bside.study.security.UserPrincipal;
-import com.bside.study.user.entity.User;
 import com.bside.study.user.dto.UserInfoResponse;
+import com.bside.study.user.entity.User;
 import com.bside.study.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.modelmapper.ModelMapper;
+import org.springframework.security.access.AccessDeniedException;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Objects;
 
 import static com.bside.study.common.api.ApiUtils.success;
 
@@ -22,12 +28,25 @@ public class UserController {
 
     private final ModelMapper modelMapper;
     private final UserRepository userRepository;
+    private final GoalAvailableTimeRepository goalAvailableTimeRepository;
 
-    @GetMapping("/users/me")
-    public ApiResult<UserInfoResponse> getCurrentUser(@CurrentUser UserPrincipal userPrincipal) {
+    @GetMapping("/users/{userId}/me")
+    public  ApiResult<UserInfoResponse> getCurrentUser(
+            @CurrentUser UserPrincipal userPrincipal,
+            @PathVariable Long userId) {
+
+        if (!Objects.equals(userPrincipal.getId(), userId)) {
+            throw new AccessDeniedException("잘못된 접근입니다.");
+        }
+
         User user = userRepository.findById(userPrincipal.getId())
                 .orElseThrow(() -> new ResourceNotFoundException("User", "id", userPrincipal.getId()));
 
-        return success(modelMapper.map(user, UserInfoResponse.class));
+        GoalAvailableTime goalAvailableTime = goalAvailableTimeRepository.findByUserId(userId);
+
+        UserInfoResponse userInfoResponse = modelMapper.map(user, UserInfoResponse.class);
+        userInfoResponse.calcTotalGoalAvailableTime(goalAvailableTime.getDaily(), goalAvailableTime.getWeekly());
+
+        return success(userInfoResponse);
     }
 }
--- src/main/java/com/bside/study/user/dto/UserInfoResponse.java
+++ src/main/java/com/bside/study/user/dto/UserInfoResponse.java
@@ -9,4 +9,9 @@ public class UserInfoResponse {
     private Long id;
     private String name;
     private String email;
+    private int total;
+
+    public void calcTotalGoalAvailableTime(int daily, int weekly) {
+        total = (daily * 5) + (weekly * 2);
+    }
 }
