ff24443084e28ab54785527e0fead7047a420789
seonghyeoklee <dltjdgur327@gmail.com>
2022-05-22 18:59:25 +0900

    feat : 마이페이지 수정

--- src/main/java/com/bside/study/goal/repository/GoalCalendarRepositoryCustom.java
+++ src/main/java/com/bside/study/goal/repository/GoalCalendarRepositoryCustom.java
@@ -4,4 +4,6 @@ import com.bside.study.goal.entity.GoalCalendar;
 
 public interface GoalCalendarRepositoryCustom {
     GoalCalendar findByGoalDetailIdAndGoalDate(Long goalDetailId, String date);
+
+    Long countGoalCalendarByUserIdAndCompleted(Long userId);
 }
